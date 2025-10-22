3d86b3155d6d43e38e6db61ce071c627562a248e
Marcus Spear <92119665+mspear299@users.noreply.github.com>
2025-10-21 23:20:36 -0400

    Update MyBinaryTree.java

--- MyBinaryTree.java
+++ MyBinaryTree.java
@@ -1,5 +1,3 @@
-
-
 import java.util.*;
 
 /**Creates a MyBinaryTree object that stores unsorted nodes in a binary tree.
@@ -8,76 +6,108 @@ import java.util.*;
  * @version October 21, 2025
  */
 public class MyBinaryTree<E extends Comparable<E>> {
-    
-    /**Root Node of the tree*/
-    protected Node<E> root;
-    
-    /**Nested class for a Node object
-     * @param <E> the data type of the element in the node
-     */
-    protected static class Node<E>{
-        E data;
-        Node<E> left;
-        Node<E> right;
-        
-        Node(E data){
-            this.data = data;
-        }
-    }
-    
-    /**Recursively searches for the next available insertion spot
-     * and inserts a new node with the given value to that space
-     * in the tree.
-     * @param value - the value to insert into the tree.
-     */
-    public void insert(E value) {
-        root = insertRecursively(root, value);
-    }
 
-    /** Recursive helper for insertion.
-     * @param current - the current Node being evaluated.
-     * @param value - the value to insert into the tree.
-     */
-    private Node<E> insertRecursively(Node<E> current, E value) {
-        if (current == null) {
-            return new Node<>(value);
-        }
-        if (value.compareTo(current.data) < 0) {
-            current.left = insertRecursively(current.left, value);
-        } else if (value.compareTo(current.data) > 0) {
-            current.right = insertRecursively(current.right, value);
-        }
-        return current;
-    }
-    
-    /** Helper function to quickly build a tree from a List of elements.
-     * @param elements - a List of data type E to be added to the tree.
-     */
-    public void buildTree(List<E> elements) {
-        for (E element : elements) {
-            insert(element);
-        }
-    }
-    
-    /**
-     * Function to print each node in the order they're encountered in by level
-     * @author Marcus Spear
-     */
-    public void breadthFirstSearch(){
-        if(root==null){
-            return;
-        }
-        Queue<Node<E>>queue=new ArrayDeque<Node<E>>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            Node<E> node=queue.poll();
-            System.out.println(node.data +" ");
-            if(node.left != null){
-                queue.add(node.left);
-            }
-            if(node.right != null){
-                queue.add(node.right);
-            }
-        }
-    }
+	/**Root Node of the tree*/
+	protected Node<E> root;
+
+	/**Nested class for a Node object
+	 * @param <E> the data type of the element in the node
+	 */
+	protected static class Node<E>{
+		E data;
+		Node<E> left;
+		Node<E> right;
+
+		Node(E data){
+			this.data = data;
+		}
+	}
+
+	/**Recursively searches for the next available insertion spot
+	 * and inserts a new node with the given value to that space
+	 * in the tree.
+	 * @param value - the value to insert into the tree.
+	 */
+	public void insert(E value) {
+		root = insertRecursively(root, value);
+	}
+
+	/** Recursive helper for insertion.
+	 * @param current - the current Node being evaluated.
+	 * @param value - the value to insert into the tree.
+	 */
+	private Node<E> insertRecursively(Node<E> current, E value) {
+		if (current == null) {
+			return new Node<>(value);
+		}
+		if (value.compareTo(current.data) < 0) {
+			current.left = insertRecursively(current.left, value);
+		} else if (value.compareTo(current.data) > 0) {
+			current.right = insertRecursively(current.right, value);
+		}
+		return current;
+	}
+
+	/** Helper function to quickly build a tree from a List of elements.
+	 * @param elements - a List of data type E to be added to the tree.
+	 */
+	public void buildTree(List<E> elements) {
+		for (E element : elements) {
+			insert(element);
+		}
+	}
+
+	/**
+	 * Function to print each node in the order they're encountered in by level
+	 * @author Marcus Spear
+	 */
+	public void breadthFirstSearch(){
+		if(root==null){
+			return;
+		}
+		Queue<Node<E>>queue=new ArrayDeque<Node<E>>();
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			Node<E> node=queue.poll();
+			System.out.println(node.data +" ");
+			if(node.left != null){
+				queue.add(node.left);
+			}
+			if(node.right != null){
+				queue.add(node.right);
+			}
+		}
+	}
+
+	/**
+	 * Searches the tree for the given target value.
+	 * @param target - the value to search for in the tree
+	 * @return true if the target is found, false otherwise
+	 */
+	public boolean find(E target) {
+		return find(root, target);
+	}
+
+	/**
+	 * Recursive helper for find.
+	 * @param localRoot - the current Node being checked
+	 * @param target - the value to search for in the tree
+	 * @return true if the target is found, false otherwise
+	 */
+	private boolean find(Node<E> localRoot, E target) {
+		if (localRoot == null)
+			return false;
+
+		// Compare the target with the data field at the root.
+		int compResult = target.compareTo(localRoot.data);
+
+		if (compResult == 0)
+			return true;
+		else if (compResult < 0)
+			return find(localRoot.left, target);
+		else
+			return find(localRoot.right, target);
+	}
 }
+
+
