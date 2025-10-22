f052f52f60473b58b26a5793bfbe6f5e3d5045f2
Kareem Niare <kareem.niare@maine.edu>
2025-10-21 22:07:16 -0400

    Update MyBinaryTree.java

--- MyBinaryTree.java
+++ MyBinaryTree.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 /**Creates a MyBinaryTree object that stores unsorted nodes in a binary tree.
  * @param <E> the data type of the elements in the tree
- * @author Abby Pitcairn
- * @version October 18, 2025
+ * @author Abby Pitcairn, Kareem Niare, & Marcus Spear
+ * @version October 21, 2025
  */
 public class MyBinaryTree<E extends Comparable<E>> {
     
@@ -58,4 +58,34 @@ public class MyBinaryTree<E extends Comparable<E>> {
             insert(element);
         }
     }
+
+    /**
+     * Searches the tree for the given target value.
+     * @param target - the value to search for in the tree
+     * @return true if the target is found, false otherwise
+     */
+    public boolean find(E target) {
+    	return find(root, target);
+    }
+
+    /**
+     * Recursive helper for find.
+     * @param localRoot - the current Node being checked
+     * @param target - the value to search for in the tree
+     * @return true if the target is found, false otherwise
+     */
+	private boolean find(Node<E> localRoot, E target) {
+		if (localRoot == null)
+			return false;
+		
+		// Compare the target with the data field at the root.
+		int compResult = target.compareTo(localRoot.data);
+		
+		if (compResult == 0)
+			return true;
+		else if (compResult < 0)
+			return find(localRoot.left, target);
+		else
+			return find(localRoot.right, target);
+	}
 }
